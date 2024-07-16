82f91721c5ed519b28639aaf8579d5e22b18ed9a
AlexRussDev <107403201+AlexRussDev@users.noreply.github.com>
2024-07-16 21:10:01 +0300

    Update Filter.java

--- src/main/java/com/askend/filtersdemo/entity/Filter.java
+++ src/main/java/com/askend/filtersdemo/entity/Filter.java
@@ -11,7 +11,11 @@ public class Filter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 1, max = 50, message = "Name must be between 1 and 50 characters")
     private String name;
+    
     private String criteria;
     private String selection;
 
